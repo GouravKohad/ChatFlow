15c7e033a3f66b4ed3dea68d88ba73abd0cad08a
Gourav Kuhad Saini <133871635+GouravKohad@users.noreply.github.com>
2025-09-07 11:22:13 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -60,20 +60,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
 
-  app.get('/sitemap.xml', (req: Request, res: Response) => {
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-   <url>
-       <loc>https://chatflow23.onrender.com/</loc>
-       <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-       <changefreq>daily</changefreq>
-       <priority>1.0</priority>
-   </url>
-</urlset>`;
 
-    res.header('Content-Type', 'application/xml');
-    res.send(sitemap);
-});
 
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
